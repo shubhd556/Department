8aa93b0f5d25413b6c27a1f614c6c703c87fd34b
unknown <shubhamdudhal9@gmail.com>
2021-08-25 17:05:30 +0530

    Updated Files and Data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Lumia - v2.2.1
-* Template URL: https://bootstrapmade.com/lumia-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
